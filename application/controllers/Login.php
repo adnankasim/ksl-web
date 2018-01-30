28afd16ccf83a957f8f71e2de2b845a98742d903
Adnan Kasim <adnankasim8@gmail.com>
2018-01-30 13:15:41 +0800

    menghapus password pada controller login & perbaikan Readme

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,9 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-// admin : admin | admin ksl-ung
-// operator : hydra | hidayatchandra
-
   class Login extends CI_Controller{
     public function __construct(){
       parent::__construct();
